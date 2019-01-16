27b24c5bbd5a922038c777a0ef377940af259b40
Adrian Bürki <adrian.buerki@students.fhnw.ch>
2019-01-16 19:02:53 +0100

    repo test

---
+++ P3-Museum_Nostrum/Assets/Main/Scripts/RepoTestSCrip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepoTestSCrip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Hoi Stefan
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
