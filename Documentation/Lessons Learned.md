e416af38107ac3c0f40295f966a0a0288576b1c6
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2019-01-18 17:28:42 +0100

    added aspect ratio handling to image prefab. Issue left: Other ratios aside from 1:1 and 2:1 or 1:2 aren't working yet.

--- Documentation/Lessons Learned.md
+++ Documentation/Lessons Learned.md
@@ -6,3 +6,13 @@
 ## Tag Comparison: Strings vs. Enum
 It is faster to compare enum values than strings. Unity offers a string based tags to identify objects. But enums can be compared with a noticable performance difference. One open question is left to ask: How performant is GetComponent<MyCustomEnumTag> to access a GameObjects custom tag(s)?
 - [Squeezing out Performance: Comparing objects](https://forum.unity.com/threads/squeezing-out-performance-comparing-objects.178593/)
+
+
+## Observer Pattern in Unity
+Demonstrated at an InputManager
+- [How to manage input state in Unity3D?](https://gamedev.stackexchange.com/questions/65957/how-to-manage-input-state-in-unity3d)
+
+
+## Locking orientations for mobile devies
+Edit -> Project Settings -> Player. Then under "Resolution and Presentation" group you can see the "Orientation" sub group. Here you can toggle off the undesired orientations.
+- [Android: How do I force only portrait orientation?](https://answers.unity.com/questions/615249/android-how-do-i-force-only-portrait-orientation.html)
---
+++ P3-Museum_Nostrum/Assets/Main/Scripts/ImageResolutionHandling/PictureResolutionHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureResolutionHandler : MonoBehaviour {
+
+    //todo: Implement interface (and abstract class) in case image frame and UISlots get resizing too.
+
+    //common image ratios for photographs: https://www.shutterstock.com/blog/common-aspect-ratios-photo-image-sizes
+    //- 16:9
+    //- 4:3
+    //- 3:2 Note: Might not work due to unity's import settings. Todo: How to keep the aspect ratio of source image?
+    //- 4:3
+    //- 1:1
+    //
+
+    
+    [SerializeField][Tooltip("Scales this picture plane's width and height measured in unity units.")]
+    private float baseScale = 0.01f;
+
+    private float baseHeightForLandscape = 9f;
+    private float baseheightForPortrait = 16f;
+
+    [SerializeField][Tooltip("If checked, allows you to constantly update the aspect ratio of this gameobject.")]
+    private bool debugging = false;
+
+    // Use this for initialization
+    void Start () {
+        if (!debugging) Rescale();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (debugging) Rescale();
+	}
+
+    private void Rescale()
+    {
+        Texture t = GetTexture();
+        //How much taller is the width compared to height?
+        float aspectRatio = t.width / (t.height * 1.0f);
+        Vector3 scale = transform.localScale;
+        Debug.Log("width " + t.width + " / height " + t.height + " = ratio: " + aspectRatio);
+        scale.x = CalculateWidth(aspectRatio);
+        scale.z = CalculateHeight(aspectRatio);
+        Debug.Log("Scale: " + scale);
+        transform.localScale = scale;
+    }
+
+    private float CalculateHeight(float aspectRatio)
+    {
+        return DetermineHeight(aspectRatio) * baseScale;
+    }
+
+    private float CalculateWidth(float aspectRatio)
+    {
+        return aspectRatio * DetermineHeight(aspectRatio) * baseScale;
+    }
+
+    private float DetermineHeight(float aspectRatio)
+    {
+        // widthxheight
+        // 2048x1024 -> 2/1  = 2        => Querformat
+        // 1024x2048 -> 1/2  = 0.5      => Hochformat
+        // 1000x1500 -> 2/3  = 0.666    => Hochformat
+        // 1500x1000 -> 3/2 = 1.5       => Querformat
+        // 1024x1024 -> 1/1 = 1         => Quadratisch
+        if (aspectRatio > 1f)
+        {
+            return baseHeightForLandscape;
+        }
+        else
+        {
+            return baseheightForPortrait;
+        }
+    }
+
+    private Texture GetTexture()
+    {
+        return GetComponent<Renderer>().material.mainTexture;
+    }
+
+    public void SetTexture(Texture t)
+    {
+        GetComponent<Renderer>().material.mainTexture = t;
+        Rescale();
+    }
+}
