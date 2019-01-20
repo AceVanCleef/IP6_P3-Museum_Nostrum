21d569c4d7ca85ec71f80e31e891ae59f61a6989
Adrian Bürki <adrian.buerki@students.fhnw.ch>
2019-01-20 15:12:15 +0100

    finished EntranceAnimation

---
+++ P3-Museum_Nostrum/Assets/Main/Scripts/Animations/StartEntranceAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEntranceAnimation : MonoBehaviour {
+
+    public Animator anim1;
+
+    void Start()
+    {
+        anim1 = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            anim1.Play("EntranceAnimation");
+        }
+    }
+}
---
+++ P3-Museum_Nostrum/Assets/Main/Scripts/Animations/StartFadeAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFadeAnimation : MonoBehaviour {
+    public Animator anim1;
+
+    void Start()
+    {
+        anim1 = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            anim1.Play("FadeAnimation");
+        }
+    }
+}
