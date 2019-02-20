1e7c39295912378674d90213326e2f8a59f8023a
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2019-02-20 21:41:30 +0100

    added ITagEnsurance impplementations to BackgroundMusic, WayFinder_Holder and InteriorHolder

---
+++ P3-Museum_Nostrum/Assets/Main/Scripts/Configuration/SignPostHolderConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignPostHolderConfig : MonoBehaviour, ITagEnsurance {
+
+    void Start()
+    {
+        InitializeTag();
+    }
+
+    public void InitializeTag()
+    {
+        if (gameObject.tag != "WayfindingHolder")
+        {
+            gameObject.tag = "WayfindingHolder";
+        }
+    }
+}
