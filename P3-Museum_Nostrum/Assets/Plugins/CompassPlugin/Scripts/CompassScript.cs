88ca1d4b8d00c5e180c161fcbcce9eb494c14fe8
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2019-02-28 11:04:34 +0100

    changed compass so that the blue marker gets rotated.

--- P3-Museum_Nostrum/Assets/Plugins/CompassPlugin/Scripts/CompassScript.cs
+++ P3-Museum_Nostrum/Assets/Plugins/CompassPlugin/Scripts/CompassScript.cs
@@ -6,62 +6,47 @@ using UnityEngine.UI;
 public class CompassScript : MonoBehaviour {
 
     /// <summary>
-    /// Conceptual idea: rotate compass image "direction needle" according to main camera rotation. 
-    /// Since the main camera is a child of player GO, the player GO's transform.rotation.y value has 
-    /// to be taken for evaluating in which direction the player is currently looking at (N, NE, E, SE,...).
-    /// 
-    /// Default values:
-    /// 0° = North
-    /// 90° = East
-    /// 100° = South
-    /// 270° = West
-    /// 
-    /// Since n * 360° + 90° = East, the calculation has to be made using modulo.
-    /// E.g. 
-    /// 450° mod 360° = 90°
-    /// 900° mod 360° = 180°
-    /// ...
-    /// For negative values:
-    /// -90° mod 360° = -90° => 270°
-    /// -450° mod 360° = -90° => 270°
-    /// ...these values have to be substracted from 360° to evaluate the positive value.
-    /// 
-    /// 
-    /// Customization feature: Manually define what angle is considered north, east, south and west:
-    /// 0° + Offset = North
-    /// 90° + Offset = East
-    /// 100° + Offset = South
-    /// 270° + Offset = West
-    /// 
-    /// The image "direction needles" angle can be set by setting its RectTransform.rotation.z value.
-    /// See RotateCompass() for its implementation. Unity offers a transformation between Quaternions and Euler Angles thus
-    /// enabling us to rotate using degrees.
-    /// When using Euler angles, the modulo calculation will be handled by Unity.
+    /// stores the source of the rotation.
     /// </summary>
-    /// 
-
-    //the source of the rotation.
     private Transform playerTransform;
 
     //the target of the rotation.
-    private RectTransform directionNeedleRT;
+    /// <summary>
+    /// stores the target of the rotation.
+    /// </summary>
+    private RectTransform directionMarker;
 
     [Tooltip("Defines how many degrees north offsets from 0°.")]
     public float Offset = 0f;
 
+    [Tooltip("Adjust the rotation to clockwise or counter-clockwise. " +
+        "Note: must be set up before launching the game.")]
+    public bool invertRotationDirection = true;
+    private float invertValue;  //either 1f or -1f.
+
+
     void Start () {
         //Get the source of the rotation, namely the player GameObject.
-        playerTransform = Camera.main.GetComponentInParent<Transform>();
+        playerTransform = Camera.main.GetComponent<Transform>();
 
         //Find RectTransform of image named "Direction needle"...
         RectTransform[] rectTransforms = GetComponentsInChildren<RectTransform>();
         int i = 0;
-        while (i < rectTransforms.Length && rectTransforms[i].name != "Direction needle")
+        while (i < rectTransforms.Length && rectTransforms[i].name != "Background")
         {
             ++i;
         }
         //...and allocate the target for the rotation.
-        directionNeedleRT = rectTransforms[i];
+        directionMarker = rectTransforms[i];
+
+        if (invertRotationDirection)
+        {
+            invertValue = -1f;
+        }
+        else
+        {
+            invertValue = 1f;
+        }
 	}
 	
 	void Update () {
@@ -70,9 +55,9 @@ public class CompassScript : MonoBehaviour {
 
     private void RotateCompass()
     {
-        Vector3 compassRotation = directionNeedleRT.rotation.eulerAngles;
-        compassRotation.z = CalculateAngle();
-        directionNeedleRT.rotation = Quaternion.Euler(compassRotation);
+        Vector3 compassRotation = directionMarker.rotation.eulerAngles;
+        compassRotation.z = CalculateAngle() * invertValue;
+        directionMarker.rotation = Quaternion.Euler(compassRotation);
     }
     
     private float CalculateAngle()
