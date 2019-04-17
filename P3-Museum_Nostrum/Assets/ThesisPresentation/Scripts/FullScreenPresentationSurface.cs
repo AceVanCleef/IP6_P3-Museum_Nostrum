d32e19f3ed583452b78e77263e7e1d5ccf98355b
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2019-04-17 16:25:03 +0200

    added presentation scene

---
+++ P3-Museum_Nostrum/Assets/ThesisPresentation/Scripts/FullScreenPresentationSurface.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FullScreenPresentationSurface : MonoBehaviour, IPointerClickHandler
+{
+    private static FullScreenPresentationSurface instance = null;
+
+    private RawImage rw;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("hitting rw");
+        if (IsShowing())
+        {
+            Hide();
+        }
+    }
+
+
+    private void Init()
+    {
+        rw.color = new Color(rw.color.r, rw.color.g, rw.color.b, 0f);
+        rw.texture = null;
+        rw.raycastTarget = false;
+    }
+
+    public void Show(Texture t)
+    {
+        rw.color = new Color(rw.color.r, rw.color.g, rw.color.b, 1f);
+        rw.texture = t;
+        rw.raycastTarget = true;
+    }
+
+    public void Hide()
+    {
+        rw.color = new Color(rw.color.r, rw.color.g, rw.color.b, 0f);
+        rw.texture = null;
+        rw.raycastTarget = false;
+    }
+
+    public bool IsShowing()
+    {
+        return rw.texture;
+    }
+
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rw = GetComponent<RawImage>();
+        Init();
+
+    }
+}
---
+++ P3-Museum_Nostrum/Assets/ThesisPresentation/Scripts/PresentationSheet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PresentationSheet : MonoBehaviour, IPointerClickHandler
+{
+    private FullScreenPresentationSurface fsps;
+    private Texture t;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!fsps.IsShowing())
+        {
+            fsps.Show(t);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        fsps = UnityEngine.Object.FindObjectOfType<FullScreenPresentationSurface>();
+        t = GetComponent<Renderer>().material.mainTexture;
+    }
+	
+	
+}
