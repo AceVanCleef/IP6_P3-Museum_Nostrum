a583efe797f3a6b77c3e84001fdbc26fe0602153
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2019-04-21 18:18:27 +0200

    creating presentaiton rooms for ausgangslage and vorgehen+methodik

--- P3-Museum_Nostrum/Assets/ThesisPresentation/Scripts/CameraZoomManager.cs
+++ P3-Museum_Nostrum/Assets/ThesisPresentation/Scripts/CameraZoomManager.cs
@@ -8,6 +8,8 @@ public class CameraZoomManager : MonoBehaviour {
 
     private Transform mainCamTransform;
     private Vector3 originalPos;
+    private Transform player;
+    private Vector3 prevPlayerPos;
 
     private float transitionDuration = 1.0f;
 
@@ -18,13 +20,8 @@ public class CameraZoomManager : MonoBehaviour {
 
     public IEnumerator MoveCameraTo(Vector3 targetPos)
     {
-
-        Debug.Log("MoveCamTo? " + mainCamTransform.name);
         //make sure that the camera stays in front of the image.
-
         yield return StartCoroutine(LerpToPosition(LerpSpeed, GetPositionInfrontOfSheet(1.25f, targetPos)));
-
-        Debug.Log("done zooming");
     }
 
     private Vector3 GetPositionInfrontOfSheet(float dist, Vector3 originalTargetPos)
@@ -43,22 +40,35 @@ public class CameraZoomManager : MonoBehaviour {
         return originalTargetPos;
     }
 
-    IEnumerator LerpToPosition(float lerpSpeed, Vector3 newPosition)
+    IEnumerator LerpToPosition(float lerpSpeed, Vector3 newPosition, bool localPosition = false)
     {
         Debug.Log("LerpCamTo?");
         float t = 0.0f;
+        
         Vector3 startingPos = mainCamTransform.position;
+        if (localPosition) //override if necessary.
+        {
+            startingPos = mainCamTransform.localPosition;
+        }
+
         while (t < 1.0f)
         {
             t += Time.deltaTime * (Time.timeScale / lerpSpeed);
-            mainCamTransform.position = Vector3.Lerp(startingPos, newPosition, t);
+            if (localPosition)
+            {
+                mainCamTransform.localPosition = Vector3.Lerp(startingPos, newPosition, t);
+            }
+            else
+            {
+                mainCamTransform.position = Vector3.Lerp(startingPos, newPosition, t);
+            }
             yield return new WaitForEndOfFrame();
         }
     }
 
     public void ResetCamera()
     {
-        StartCoroutine(LerpToPosition(LerpSpeed, originalPos));
+        StartCoroutine(LerpToPosition(LerpSpeed, originalPos, true));
     }
 
     private void Awake()
@@ -76,7 +86,18 @@ public class CameraZoomManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
         mainCamTransform = Camera.main.transform;
-        originalPos = mainCamTransform.position;
+        //player = UnityEngine.Object.FindObjectOfType<PlayerRotator>().transform;
+        originalPos = mainCamTransform.localPosition;
+        //prevPlayerPos = player.position;
 	}
+    /*
+    private void Update()
+    {
+        if (Vector3.Distance(player.position, prevPlayerPos) > float.Epsilon)
+        {
+            prevPlayerPos = player.position;
+            originalPos = mainCamTransform.localPosition;
+        }
+    }*/
 
 }
